e94d91ccd7c969ff3f3ba57305766e4cee605367
BrenoCVS <brenocvsilva14@gmail.com>
2025-06-05 18:56:26 -0300

    Mudando organização de arquivos

--- reconhecimentoFacial/exemplo02/exemplo02.py
+++ reconhecimentoFacial/exemplo02/exemplo02.py
@@ -2,7 +2,7 @@
 
 import cv2
 
-face_cascade = cv2.CascadeClassifier('../../haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('../haarcascade_frontalface_default.xml')
 
 img = cv2.imread('image/foto.jpeg')
 
